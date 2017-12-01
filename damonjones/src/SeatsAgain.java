da58f03840e2c0a1e445e6bfb05c5fea71dccbfd
Damon Jones <damonjones321@ksu.edu>
2017-12-01 17:43:02 -0600

    Fixed it

--- damonjones/src/SeatsAgain.java
+++ damonjones/src/SeatsAgain.java
@@ -1,10 +1,23 @@
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.TextArea;
+import javax.swing.JList;
 
 public class SeatsAgain {
 
 	private JFrame frame;
+	private ArrayList<String>selectedSeats = new ArrayList<String>();
+	private ArrayList<JButton>b = new ArrayList<JButton>();
 
 	/**
 	 * Launch the application.
@@ -33,9 +46,90 @@ public class SeatsAgain {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		int seatsx=10;
+		int seatsy=5;
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
+		frame.getContentPane().setLayout(null);
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel);
+		panel.setBounds(15, 16, 296, 174);
+		panel.setLayout(new GridLayout(seatsx, seatsy, 0, 0));
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(310, 16, 103, 38);
+		frame.getContentPane().add(panel_1);
+		
+		int seatASCII = 65;
+		int seatNum = 1;
+		char seatLetter = (char)seatASCII;
+		//JButton[][] b = new JButton[seatsx][seatsy];
+		int num = 0;
+		
+		for(int i=0; i<seatsx;i++) {
+			for(int j = 0; j<seatsy;j++) {
+				StringBuilder sb = new StringBuilder();
+				sb.append(seatLetter);
+				sb.append(seatNum);
+				b.add(new JButton(sb.toString()));
+				b.get(num).setBackground(Color.gray);
+				b.get(num).addActionListener(new ActionListener(){
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						if(((JButton)e.getSource()).getBackground()==Color.gray) {
+							((JButton)e.getSource()).setBackground(Color.green);
+						} else {
+							((JButton)e.getSource()).setBackground(Color.gray);
+						}
+					}
+				});
+				panel.add(b.get(num));
+				seatNum++;
+				num++;
+			}
+			seatNum = 1;
+			seatASCII++;
+			seatLetter = (char)seatASCII;
+		}
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBounds(15, 190, 398, 38);
+		frame.getContentPane().add(panel_2);
+		
+		JLabel customerSeats = new JLabel();
+		
+		JButton btnsubmit = new JButton("Submit");
+		btnsubmit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				for(int i = 0; i<b.size();i++) {
+					if(b.get(i).getBackground()==Color.green) {
+						selectedSeats.add(b.get(i).getText());
+						//System.out.println(b.get(i).getText());
+						
+					}
+				}
+				StringBuilder sb = new StringBuilder();
+				for(int i = 0; i<selectedSeats.size(); i++) {
+					sb.append(selectedSeats.get(i)+"  ");
+				}
+				customerSeats.setText(sb.toString());
+				JButton jb = (JButton)e.getSource();
+				jb.setEnabled(false);
+				for(int i = 0; i<b.size();i++) {
+					b.get(i).setEnabled(false);
+				}
+				
+				
+			}
+		});
+		
+		panel_1.add(btnsubmit);
+		panel_2.add(customerSeats);
+		
+		
 
+		
+	}
 }
